b163fccf317af376c0a7d665f5d2ff04daf52d15
Daniel Naab <danielnaab@gmail.com>
2014-08-08 18:02:03 -0500

    Minimize all resources with the "dist" task.

--- gulpfile.js
+++ gulpfile.js
@@ -6,22 +6,22 @@ var _ = require('lodash'),
     connect = require('gulp-connect'),
     es6Browserify = require('es6-browserify'),
     gulp = require('gulp'),
+    gulpif = require('gulp-if'),
     gutil = require('gulp-util'),
     imagemin = require('gulp-imagemin'),
     jsxify = require('mercury-jsxify'),
     minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename'),
     rimraf = require('gulp-rimraf'),
     sass = require('gulp-sass'),
     size = require('gulp-size'),
     source = require('vinyl-source-stream'),
+    streamify = require('gulp-streamify'),
+    uglify = require('gulp-uglify'),
     useref = require('gulp-useref'),
     watchify = require('watchify')
 
-var DEBUG = true
 
-
-function scripts(watch) {
+function scripts(watch, debug) {
     var args
     if (watch) {
         args = _.clone(watchify.args)
@@ -29,7 +29,7 @@ function scripts(watch) {
     else {
         args = {}
     }
-    args.debug = DEBUG
+    args.debug = debug
 
     var bundler = browserify('./app/scripts/app.js', args)
         .transform(jsxify, {es6: true})
@@ -42,7 +42,7 @@ function scripts(watch) {
     bundler.transform(jsxify)
 
     function rebundle() {
-        gutil.log('(watchify) rebundling... ')
+        gutil.log('Bundling... ')
 
         return bundler.bundle()
             // log errors if they happen
@@ -50,6 +50,7 @@ function scripts(watch) {
                 gutil.log('Browserify Error', e)
             })
             .pipe(source('app.js'))
+            .pipe(gulpif(!debug, streamify(uglify())))
             .pipe(gulp.dest('./dist/scripts'))
             .pipe(connect.reload())
     }
@@ -60,7 +61,7 @@ function scripts(watch) {
 
 
 gulp.task('clean', function () {
-    return gulp.src(['dist/**'], {read: false})
+    return gulp.src(['dist'], {read: false})
         .pipe(rimraf())
 })
 
@@ -74,7 +75,7 @@ gulp.task('connect', function() {
 })
 
 
-gulp.task('html', function () {
+gulp.task('html', ['clean'], function () {
     return gulp.src('app/*.html')
         .pipe(useref())
         .pipe(gulp.dest('dist'))
@@ -83,7 +84,7 @@ gulp.task('html', function () {
 })
 
 
-gulp.task('images', function () {
+gulp.task('images', ['clean'], function () {
     return gulp.src('app/images/**/*')
         .pipe(cache(imagemin({
             optimizationLevel: 3,
@@ -96,26 +97,27 @@ gulp.task('images', function () {
 });
 
 
-gulp.task('scripts', function() {
-    return scripts(false)
-})
-
-
-gulp.task('styles', function () {
-    return gulp.src('./app/styles/main.scss')
+function styles(debug) {
+    return gulp.src('./app/styles/app.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./dist/styles'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(minifycss())
+        .pipe(gulpif(!debug, minifycss()))
         .pipe(gulp.dest('./dist/styles'))
         .pipe(size())
-        .pipe(connect.reload())
+}
+
+
+gulp.task('dist', ['clean', 'html'], function() {
+    styles(false)
+    return scripts(false, false)
 })
 
 
-gulp.task('watch', ['html', 'styles', 'connect'], function() {
+gulp.task('watch', ['html', 'images', 'connect'], function() {
+    gulp.watch('app/*.html', ['html']);
     gulp.watch('app/styles/**/*.scss', ['styles'])
     gulp.watch('app/image/**/', ['images'])
 
-    return scripts(true)
+    var debug = true
+    styles(debug)
+    return scripts(true, debug)
 })
